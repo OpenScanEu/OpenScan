be2415fd40111e297c874da935dfef48fb149837
Thomas <57842400+OpenScanEu@users.noreply.github.com>
2021-05-26 12:09:32 +0200

    Update app.py

--- MultiRig/Client/files/app.py
+++ MultiRig/Client/files/app.py
@@ -78,7 +78,7 @@ def setSettings(setting,value):
 #################### UPDATE ##################################
 @app.route('/getUpdate')
 def getUpdate():
-    os.system('wget https://raw.githubusercontent.com/OpenScanEu/OpenScan/master/MultiRig/Client/dependencies.py -O dependencies.py && wget https://raw.githubusercontent.com/OpenScanEu/OpenScan/master/MultiRig/Client/updater.py -O updater.py && python dependencies.py && python updater.py'
+    os.system('wget https://raw.githubusercontent.com/OpenScanEu/OpenScan/master/MultiRig/Client/dependencies.py -O dependencies.py && wget https://raw.githubusercontent.com/OpenScanEu/OpenScan/master/MultiRig/Client/updater.py -O updater.py && python dependencies.py && python updater.py')
     os.system('sudo reboot -h now')
     return
     
