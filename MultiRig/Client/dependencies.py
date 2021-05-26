1f1a555124947eb56305f74eb0453ee28f9f4fb3
Thomas <57842400+OpenScanEu@users.noreply.github.com>
2021-05-26 10:58:25 +0200

    Create dependencies.py

---
+++ MultiRig/Client/dependencies.py
@@ -0,0 +1,13 @@
+import os
+
+def installDependencies():
+    os.system('sudo apt-get update')
+    os.system('sudo raspi-config nonint do_camera 0')
+    os.system('sudo raspi-config nonint do_memory_split 256')
+    os.system('sudo apt -y install python3-pip python-pip')
+    os.system('pip install picamera')
+    os.system('pip3 install picamera flask')
+    os.system('sudo apt-get -y install python3-rpi.gpio')
+    os.system('sudo apt-get -y install zip unzip')
+    
+installDependencies()
